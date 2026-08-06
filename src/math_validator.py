0855ece350225166a8717843aea04e5360f289d9
kuntal0324-cloud <kuntal0324@gmail.com>
2026-08-06 16:30:41 +0530

    Add main block to execute trace function

--- src/math_validator.py
+++ src/math_validator.py
@@ -3,3 +3,11 @@ from sympy import Matrix
 def trace(matrix):
 
     return Matrix(matrix).trace()
+if __name__ == "__main__":
+
+    A = [
+        [2, -1],
+        [3, 4]
+    ]
+
+    print(trace(A))
