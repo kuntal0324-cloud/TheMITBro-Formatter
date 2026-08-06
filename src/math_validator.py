fc1e2f38ffeebd4267f4556405bf244279b9b2a0
kuntal0324-cloud <kuntal0324@gmail.com>
2026-08-06 16:58:36 +0530

    Add extract_matrix function and modify main block
    Added extract_matrix function and updated main block to include a question string.

--- src/math_validator.py
+++ src/math_validator.py
@@ -3,11 +3,26 @@ from sympy import Matrix
 def trace(matrix):
 
     return Matrix(matrix).trace()
+
+def extract_matrix(question):
+    pass
+
 if __name__ == "__main__":
 
-    A = [
-        [2, -1],
-        [3, 4]
-    ]
+    question = """
+    ### Question
 
-    print(trace(A))
+    Let
+
+    $$
+    A=
+    \\begin{bmatrix}
+    2 & -1\\\\
+    3 & 4
+    \\end{bmatrix}
+    $$
+
+    Find the trace.
+    """
+
+    print(extract_matrix(question))
