2477e3325b3e7ab55df7d1ba77759f05ac121e53
kuntal0324-cloud <kuntal0324@gmail.com>
2026-08-07 23:43:52 +0530

    Add example calls to parse_number function

--- src/math_validator.py
+++ src/math_validator.py
@@ -12,6 +12,8 @@ from verifiers import *
     
 if __name__ == "__main__":
 
+    print(parse_number("5"))
+    print(parse_number("3.14"))
     print(parse_number("1/2"))
     print(parse_number("sqrt(2)"))
     print(parse_number("pi"))
