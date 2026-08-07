310bd0d334f87ee8a8e910728af4dd4fabb3dcd3
kuntal0324-cloud <kuntal0324@gmail.com>
2026-08-07 23:39:13 +0530

    Update test cases in math_validator.py

--- src/math_validator.py
+++ src/math_validator.py
@@ -12,7 +12,6 @@ from verifiers import *
     
 if __name__ == "__main__":
 
-    print(parse_number("5"))
-    print(parse_number("-7"))
-    print(parse_number("3.14"))
-    print(parse_number("-2.75"))
+    print(parse_number("1/2"))
+    print(parse_number("sqrt(2)"))
+    print(parse_number("pi"))
