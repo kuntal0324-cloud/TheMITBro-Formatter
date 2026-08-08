c8bfd9feef8be8ce2dbbea71bdefdcf15c3aea2a
kuntal0324-cloud <kuntal0324@gmail.com>
2026-08-08 07:26:15 +0530

    Modify main block to extract matrix from input
    Updated the main block to demonstrate matrix extraction.

--- src/math_validator.py
+++ src/math_validator.py
@@ -12,13 +12,14 @@ from verifiers import *
     
 if __name__ == "__main__":
 
-    try:
-        print(parse_number("abc@@"))
-    except Exception as e:
-        print("Invalid input handled:", type(e).__name__)
+    question = r"""
+    $$
+    A=
+    \begin{bmatrix}
+    1/2 & sqrt(2)\\
+    pi & -3.5
+    \end{bmatrix}
+    $$
+    """
 
-print(parse_number("1.5"))
-print(parse_number("1/2"))
-print(parse_number("sqrt(2)"))
-print(parse_number("pi"))
-
+    print("Matrix:", extract_matrix(question))
