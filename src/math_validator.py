dd7a84a582d25d9596c7f48c878a3f481274563f
kuntal0324-cloud <kuntal0324@gmail.com>
2026-08-08 08:07:23 +0530

    Refactor matrix nullspace question and add column space output
    Removed question and answer related to matrix nullspace. Added functionality to print column space of a matrix.

--- src/math_validator.py
+++ src/math_validator.py
@@ -10,36 +10,10 @@ from verifiers import *
 # Testing
 # -----------------------------------
     
-if __name__ == "__main__":
+matrix = Matrix([
+    [1, 2],
+    [2, 4]
+])
 
-    question = r"""
-### Question
-
-Let
-
-$$
-A=
-\begin{bmatrix}
-1 & 2\\
-2 & 4
-\end{bmatrix}
-$$
-
-Find the nullspace of A.
-
-**Correct Answer:**
-
-$$
-\left\{
-\begin{bmatrix}
--2\\
-1
-\end{bmatrix}
-\right\}
-$$
-
-**Concept Tested:** Matrix Nullspace
-"""
-
-    print("Matrices:", extract_matrices(question))
-    print("Verify:", verify(question))
+print("Column Space:")
+print(column_space(matrix))
