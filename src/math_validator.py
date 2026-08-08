7c6778e93edfce1a960a944558526e1e4d334835
kuntal0324-cloud <kuntal0324@gmail.com>
2026-08-08 23:38:21 +0530

    Refactor math_validator to compute matrix rank
    Removed matrix inverse question and added matrix rank computation.

--- src/math_validator.py
+++ src/math_validator.py
@@ -10,29 +10,12 @@ from verifiers import *
 # Testing
 # -----------------------------------
 
-question = r"""
-### Question
+matrix = [
+    [1, 2, 3],
+    [2, 4, 6],
+    [1, 1, 1]
+]
 
-Find the inverse of the matrix
+computed_rank = matrix_rank(matrix)
 
-$$
-A=
-\begin{bmatrix}
-4 & 7\\
-2 & 6
-\end{bmatrix}
-$$
-
-**Correct Answer:**
-$$
-A^{-1}=
-\begin{bmatrix}
-\frac{3}{5} & -\frac{7}{10}\\
--\frac{1}{5} & \frac{2}{5}
-\end{bmatrix}
-$$
-
-**Concept Tested:** Matrix Inverse
-"""
-
-print("Verify:", verify(question))
+print("Rank:", computed_rank)
