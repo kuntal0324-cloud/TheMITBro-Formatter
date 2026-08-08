041edcd2108a93f8af3e5535ec078f6dd285ce41
kuntal0324-cloud <kuntal0324@gmail.com>
2026-08-08 07:46:37 +0530

    Remove question and add matrix nullspace verification

--- src/math_validator.py
+++ src/math_validator.py
@@ -12,31 +12,10 @@ from verifiers import *
     
 if __name__ == "__main__":
 
-    question = r"""
-### Question
+    matrix = Matrix([
+        [1, 2],
+        [2, 4]
+    ])
 
-Let
-
-$$
-A=
-\begin{bmatrix}
-1 & 2\\
-2 & 4
-\end{bmatrix}
-$$
-
-Find the RREF of A.
-
-**Correct Answer:**
-
-$$
-\begin{bmatrix}
-1 & 2\\
-0 & 0
-\end{bmatrix}
-$$
-
-**Concept Tested:** Matrix RREF
-"""
-
-    print("Verify:", verify(question))
+    print("Nullspace:")
+    print(nullspace(matrix))
