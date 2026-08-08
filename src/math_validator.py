71f68897cf93b863e4d20b7ec897c6c500ffd8da
kuntal0324-cloud <kuntal0324@gmail.com>
2026-08-08 08:10:41 +0530

    Add matrix column space question and verification
    Added a question regarding the column space of a matrix with a correct answer and concept tested.

--- src/math_validator.py
+++ src/math_validator.py
@@ -10,10 +10,33 @@ from verifiers import *
 # Testing
 # -----------------------------------
     
-matrix = Matrix([
-    [1, 2],
-    [2, 4]
-])
+question = r"""
+### Question
 
-print("Column Space:")
-print(column_space(matrix))
+Let
+
+$$
+A=
+\begin{bmatrix}
+1 & 2\\
+2 & 4
+\end{bmatrix}
+$$
+
+Find a basis for the column space of A.
+
+**Correct Answer:**
+
+$$
+\left\{
+\begin{bmatrix}
+1\\
+2
+\end{bmatrix}
+\right\}
+$$
+
+**Concept Tested:** Matrix Column Space
+"""
+
+print("Verify:", verify(question))
