c4d12f2aeb520f5a5b93f8eeca667222270a87f5
kuntal0324-cloud <kuntal0324@gmail.com>
2026-08-08 09:24:04 +0530

    Remove main block and update print statement

--- src/math_validator.py
+++ src/math_validator.py
@@ -10,22 +10,7 @@ from verifiers import *
 # Testing
 # -----------------------------------
 
-if __name__ == "__main__":
-
-    question = r"""
-**Correct Answer:**
-
-Eigenvalue 2:
-\begin{bmatrix}
-1\\
-2
-\end{bmatrix}
-
-Eigenvalue 3:
-\begin{bmatrix}
-1\\
--1
-\end{bmatrix}
-"""
-
-print("Eigenvectors:", extract_eigenvectors(question))
+print(compare_eigenvectors(
+    [[1], [2]],
+    [[2], [4]]
+))
