65670e56b3dd85b71f86bf71c62655267f60c52f
kuntal0324-cloud <kuntal0324@gmail.com>
2026-08-08 20:59:57 +0530

    Add matrix diagonalization question and logic
    Added a question for matrix diagonalization and updated the matrix extraction and diagonalization logic.

--- src/math_validator.py
+++ src/math_validator.py
@@ -10,10 +10,47 @@ from verifiers import *
 # Testing
 # -----------------------------------
 
-matrix = [
-    [4, 1],
-    [2, 3]
-]
+question = r"""
+### Question
+
+Diagonalize the matrix
+
+$$
+A=
+\begin{bmatrix}
+4 & 1\\
+2 & 3
+\end{bmatrix}
+$$
+
+Find matrices P and D such that
+
+$$
+A=PDP^{-1}
+$$
+
+**Correct Answer:**
+
+P =
+$$
+\begin{bmatrix}
+-1 & 1\\
+2 & 1
+\end{bmatrix}
+$$
+
+D =
+$$
+\begin{bmatrix}
+2 & 0\\
+0 & 5
+\end{bmatrix}
+$$
+
+**Concept Tested:** Matrix Diagonalization
+"""
+
+matrix = extract_matrix(question)
 
 P, D = diagonalize_matrix(matrix)
 
@@ -25,8 +62,5 @@ print(D)
 
 print("Reconstructed:")
 print(P * D * P.inv())
-
-print("Original:")
-print(Matrix(matrix))
 
 print("Verify:", P * D * P.inv() == Matrix(matrix))
