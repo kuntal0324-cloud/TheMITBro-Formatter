a48d4ea3f4230f5c691589b5af43f3c801c422d2
kuntal0324-cloud <kuntal0324@gmail.com>
2026-08-08 09:44:42 +0530

    Update math_validator.py

--- src/math_validator.py
+++ src/math_validator.py
@@ -10,7 +10,9 @@ from verifiers import *
 # Testing
 # -----------------------------------
 
-question = r"""
+if __name__ == "__main__":
+
+    question = r"""
 ### Question
 
 Let
@@ -28,22 +30,39 @@ Find the eigenvectors of A.
 **Correct Answer:**
 
 Eigenvalue 2:
-\[
 \begin{bmatrix}
 1\\
 0
 \end{bmatrix}
-\]
 
 Eigenvalue 3:
-\[
 \begin{bmatrix}
 0\\
 1
 \end{bmatrix}
-\]
 
 **Concept Tested:** Matrix Eigenvectors
 """
 
-print("Verify:", verify_eigenvectors(question))
+    matrix = extract_matrix(question)
+    expected = extract_eigenvectors(question)
+    raw = eigenvectors(matrix)
+
+    print("Matrix:", matrix)
+    print("Expected:", expected)
+    print("Raw:", raw)
+
+    computed = {}
+
+    for eigenvalue, multiplicity, vectors in raw:
+        computed[eigenvalue] = [
+            vector.tolist()
+            for vector in vectors
+        ]
+
+    print("Computed:", computed)
+
+    print("Keys expected:", set(expected.keys()))
+    print("Keys computed:", set(computed.keys()))
+
+    print("Verify:", verify_eigenvectors(question))
