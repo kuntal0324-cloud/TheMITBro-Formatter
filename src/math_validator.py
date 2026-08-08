6df3a77f8b98d22f70efbca4d1ccc052b5889695
kuntal0324-cloud <kuntal0324@gmail.com>
2026-08-08 18:12:30 +0530

    Add main block for testing math_validator

--- src/math_validator.py
+++ src/math_validator.py
@@ -10,6 +10,12 @@ from verifiers import *
 # Testing
 # -----------------------------------
 
+if __name__ == "__main__":
+    question = r"""
+    ...
+    """  
 print("Expected:", extract_characteristic_polynomial(question))
 print("Computed:", characteristic_polynomial([[2, 0], [0, 3]]))
 print("Verify:", verify_characteristic_polynomial(question))
+
+    
