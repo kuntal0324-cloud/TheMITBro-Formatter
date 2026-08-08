aca96177b8daf4dfb86d7292b892ed780f995cea
kuntal0324-cloud <kuntal0324@gmail.com>
2026-08-08 18:36:07 +0530

    Add characteristic polynomial question to tests
    Added a question about the characteristic polynomial of a matrix to the main testing section.

--- src/math_validator.py
+++ src/math_validator.py
@@ -11,11 +11,31 @@ from verifiers import *
 # -----------------------------------
 
 if __name__ == "__main__":
-    question = r"""
-    ...
-    """  
+    ### Question
+
+Let
+
+$$
+A=
+\begin{bmatrix}
+2 & 0\\
+0 & 3
+\end{bmatrix}
+$$
+
+Find the characteristic polynomial of A.
+
+**Correct Answer:**
+
+$$
+\lambda^2-5\lambda+6
+$$
+
+**Concept Tested:** Matrix Characteristic Polynomial
+"""
+
 print("Expected:", extract_characteristic_polynomial(question))
-print("Computed:", characteristic_polynomial([[2, 0], [0, 3]]))
+print("Computed:", characteristic_polynomial(extract_matrix(question)))
 print("Verify:", verify_characteristic_polynomial(question))
 
     
