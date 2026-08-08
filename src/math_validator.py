592a8c21b46ce97e6e85f0957d3edb126007758c
kuntal0324-cloud <kuntal0324@gmail.com>
2026-08-08 18:09:32 +0530

    Remove main function and testing code
    Remove main testing block for characteristic polynomial.

--- src/math_validator.py
+++ src/math_validator.py
@@ -10,31 +10,6 @@ from verifiers import *
 # Testing
 # -----------------------------------
 
-if __name__ == "__main__":
-
-    question = r"""
-### Question
-
-Find the characteristic polynomial of
-
-$$
-A=
-\begin{bmatrix}
-2 & 0\\
-0 & 3
-\end{bmatrix}
-$$
-
-**Correct Answer:**
-
-$$
-\lambda^2-5\lambda+6
-$$
-
-**Concept Tested:** Characteristic Polynomial
-"""
-
-matrix = [[2, 0], [0, 3]]
-
-print("Characteristic Polynomial:",
-      characteristic_polynomial(matrix))
+print("Expected:", extract_characteristic_polynomial(question))
+print("Computed:", characteristic_polynomial([[2, 0], [0, 3]]))
+print("Verify:", verify_characteristic_polynomial(question))
