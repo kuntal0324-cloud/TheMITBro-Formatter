325f6c128ea6e4d3b2f329894da7814312745d34
kuntal0324-cloud <kuntal0324@gmail.com>
2026-08-08 19:09:24 +0530

    Refactor question variable and clean up comments

--- src/math_validator.py
+++ src/math_validator.py
@@ -10,9 +10,7 @@ from verifiers import *
 # Testing
 # -----------------------------------
 
-if __name__ == "__main__":
-    
-    question = r"""
+question = r"""
 ### Question
 
 Let
@@ -30,13 +28,8 @@ Find the characteristic polynomial of A.
 **Correct Answer:**
 
 $$
-\lambda^2-5\lambda+6
+\lambda^2 - 5\lambda + 6
 $$
 
 **Concept Tested:** Matrix Characteristic Polynomial
 """
-print("Expected:", extract_characteristic_polynomial(question))
-print("Computed:", characteristic_polynomial(extract_matrix(question)))
-print("Verify:", verify_characteristic_polynomial(question))
-
-    
